d05d70c7ad43e7fefc294bcbbf57367bd1bc3b9c
laidoffworkerlearnstocode <kwg6qlyv@hotmail.com>
2023-05-28 22:34:03 +0800

    02-35-38

--- simpleStorage/contracts/SimpleStorage.sol
+++ simpleStorage/contracts/SimpleStorage.sol
@@ -1,1 +1,11 @@
-pragma solidity ^0.8.7; // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7; 
+
+contract SimpleStorage {
+    // This will get initialized to 0!
+    uint256 favoriteNumber;
+    function store (uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+}
