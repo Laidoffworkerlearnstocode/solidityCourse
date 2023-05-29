e5222330d4c3838ae31b79afbd9dee542f90581e
laidoffworkerlearnstocode <kwg6qlyv@hotmail.com>
2023-05-29 09:24:01 +0800

    0:41:29

--- simpleStorage/contracts/SimpleStorage.sol
+++ simpleStorage/contracts/SimpleStorage.sol
@@ -3,9 +3,25 @@ pragma solidity ^0.8.7;
 
 contract SimpleStorage {
     // This will get initialized to 0!
-    uint256 favoriteNumber;
+    uint256 public favoriteNumber;
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+    
+    constructor () {
+        favoriteNumber = 10;
+        people.push(People(favoriteNumber, "Nathan"));
+    }
+
     function store (uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve () public view returns (uint256) {
+        return favoriteNumber;
     }
 
 }
