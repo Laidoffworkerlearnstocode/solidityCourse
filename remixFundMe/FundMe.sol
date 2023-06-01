3f26b0d29a21154a9ea862a6a21c7360a7833c97
laidOffWorkerLearnsToCode <kwg6qlyv@hotmail.com>
2023-06-01 16:57:29 +0800

    03-0:16:46

--- remixFundMe/FundMe.sol
+++ remixFundMe/FundMe.sol
@@ -8,11 +8,20 @@ contract FundMe {
     uint256 public minimumContribution = 50; // in usd
 
     function fund() public payable {
-        require(msg.value >= 0.01 ether, "Minimum contribution is 0.01 ether");
+        require(msg.value >= minimumContribution, "You need to spend more ETH!");
     }
 
-    function getPrice() public {
+    function getPrice() public view returns (uint256) {
         //0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (
+            /* uint80 roundID */,
+            int answer,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return uint256(answer * 1e10);
     }
 
     function getConversionRate() public {
