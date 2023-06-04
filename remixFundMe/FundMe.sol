01b30b2acf45cfc3084090d6bdf07db57eb68025
laidoffworkerlearnstocode <kwg6qlyv@hotmail.com>
2023-06-04 21:30:02 +0800

    04-0:32:48

--- remixFundMe/FundMe.sol
+++ remixFundMe/FundMe.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8; 
+pragma solidity ^0.8.8;
 
 import "./priceConverter.sol";
 
 contract FundMe {
-
     uint256 public minimumContribution = 50 * 1e18;
 
-    address [] public funders;
+    address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
     address public owner;
 
@@ -16,23 +15,40 @@ contract FundMe {
     }
 
     function fund() public payable {
-        require(PriceConverter.getConversionRate(msg.value) >= minimumContribution, "You need to spend more ETH!");
+        require(
+            PriceConverter.getConversionRate(msg.value) >= minimumContribution,
+            "You need to spend more ETH!"
+        );
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withDraw() public onlyOwner(){
-        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+    function withDraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
-        (bool success ,) = payable(msg.sender).call{value: address(this).balance}("");
+        (bool success, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
         require(success, "Failed to withdraw funds from contract");
     }
 
-    modifier onlyOwner {
+    modifier onlyOwner() {
         require(msg.sender == owner);
         _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 }
