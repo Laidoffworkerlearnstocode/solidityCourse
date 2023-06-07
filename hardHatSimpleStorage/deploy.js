6b03f21c87ba60263095c8bc52e3edb9bc6ee230
laidOffWorkerLearnsToCode <kwg6qlyv@hotmail.com>
2023-06-07 16:58:08 +0800

    1:00:43

--- hardHatSimpleStorage/deploy.js
+++ hardHatSimpleStorage/deploy.js
@@ -1,5 +1,6 @@
 async function main() {
-    
+    // ganache RPC Listening on 127.0.0.1:8545
+    //https://playground.open-rpc.org/?schemaUrl=https://raw.githubusercontent.com/ethereum/execution-apis/assembled-spec/openrpc.json&uiSchema%5BappBar%5D%5Bui:splitView%5D=false&uiSchema%5BappBar%5D%5Bui:input%5D=false&uiSchema%5BappBar%5D%5Bui:examplesDropdown%5D=false
 }
 
 main()
